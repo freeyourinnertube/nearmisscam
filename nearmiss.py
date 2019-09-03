bbc9af74ad0be1a0b23240271e3fffc6a39f4bd6
freeyourinnertube <54806832+freeyourinnertube@users.noreply.github.com>
2019-09-03 21:33:07 +0100

    Update nearmiss.py

--- nearmiss.py
+++ nearmiss.py
@@ -13,4 +13,87 @@
 #
 # Physical wiring
 # Put the US-100 in serial mode by enabling the jumper
+# H7 VIN -----> US-100 VCC (pin 1)
+# H7 GND -----> US-100 GND (pin 4)
+# H7 P4  -----> US-100 Trig/Tx
+# H7 P5  -----> US-100 Echo/Rx
+#
+# Proof of concept code - not optimised
+import sensor, image, time, mjpeg, pyb
+from pyb import UART
+uart = UART(3, 9600, timeout_char=1000)
+uart.init(9600, bits=8, parity=None, stop=1, timeout_char=1000)
+pyb.delay(500);
+
+RED_LED_PIN = 1
+BLUE_LED_PIN = 3
+
+sensor.reset() # Initialize the camera sensor.
+sensor.set_pixformat(sensor.RGB565) # or sensor.GRAYSCALE
+sensor.set_framesize(sensor.QVGA) # or sensor.QQVGA (or others)
+sensor.skip_frames(time = 2000) # Let new settings take affect.
+clock = time.clock() # Tracks FPS.
+
+pyb.LED(RED_LED_PIN).on()
+sensor.skip_frames(time = 2000) # Give the user time to get ready.
+
+pyb.LED(RED_LED_PIN).off()
+pyb.LED(BLUE_LED_PIN).on()
+
+m = mjpeg.Mjpeg("yourvideo.mjpeg")
+
+units = "cm"
+intdistance = 0
+lastclosepass = 200
+currentpass = 200
+
+
+print("You're on camera!")
+
+
+def get_distance():
+    command = b"\x55"
+    ch = uart.write(command)
+    ch = uart.read(2)
+    #print(ch)
+    MSByteDist = ch[0]
+    LSByteDist = ch[1]
+    mmDist  = round((MSByteDist * 256 + LSByteDist)/10)
+    #print(round(mmDist/10))
+    return mmDist
+
+
+
+#while(True):
+for i in range(240):
+
+
+    oor = False
+    distance = get_distance()
+    strdistance = str(distance)
+    clock.tick()
+
+    img = sensor.snapshot()
+
+    img.draw_rectangle(0, 0, 60, 40, color = (0, 0, 0), thickness = 0, fill = True)
+
+
+    if oor == False:
+        # Character and string rotation can be done at 0, 90, 180, 270, and etc. degrees.
+        img.draw_string(1, 1, strdistance, color = (255, 255, 255), scale = 2, mono_space = False,
+                        char_rotation = 0, char_hmirror = False, char_vflip = False,
+                        string_rotation = 0, string_hmirror = False, string_vflip = False)
+
+        img.draw_string(37, 1, units, color = (255, 255, 255), scale = 2, mono_space = False,
+                    char_rotation = 0, char_hmirror = False, char_vflip = False,
+                    string_rotation = 0, string_hmirror = False, string_vflip = False)
+
+
+    m.add_frame(img)
+    #print(clock.fps())
+
+
+m.close(clock.fps())
+pyb.LED(BLUE_LED_PIN).off()
+print("Done! Reset the camera to see the saved recording.")
 
