99fa992c10e53602e823ed41a0af93309091a3a1
Freeyourinnertube <54806832+freeyourinnertube@users.noreply.github.com>
2019-09-14 15:56:04 +0100

    Update nearmiss.py

--- nearmiss.py
+++ nearmiss.py
@@ -21,34 +21,41 @@
 # Proof of concept code - not optimised
 import sensor, image, time, mjpeg, pyb
 from pyb import UART
+from pyb import Pin
+
 uart = UART(3, 9600, timeout_char=1000)
 uart.init(9600, bits=8, parity=None, stop=1, timeout_char=1000)
 pyb.delay(500);
 
-RED_LED_PIN = 1
-BLUE_LED_PIN = 3
+
 
 sensor.reset() # Initialize the camera sensor.
 sensor.set_pixformat(sensor.RGB565) # or sensor.GRAYSCALE
 sensor.set_framesize(sensor.QVGA) # or sensor.QQVGA (or others)
+
+#comment out next 2 lines if camera lens is at top
+sensor.set_vflip(True)
+sensor.set_hmirror(True)
+
 sensor.skip_frames(time = 2000) # Let new settings take affect.
 clock = time.clock() # Tracks FPS.
-
-pyb.LED(RED_LED_PIN).on()
 sensor.skip_frames(time = 2000) # Give the user time to get ready.
 
-pyb.LED(RED_LED_PIN).off()
-pyb.LED(BLUE_LED_PIN).on()
 
-m = mjpeg.Mjpeg("yourvideo.mjpeg")
+
+
 
 units = "cm"
 intdistance = 0
+#oor = False
 lastclosepass = 200
 currentpass = 200
+recording = False
+recordingsequence = 1
+pin0 = Pin('P0', Pin.IN, Pin.PULL_DOWN)
+RED_LED_PIN = 1
 
 
-print("You're on camera!")
 
 
 def get_distance():
@@ -58,42 +65,80 @@ def get_distance():
     #print(ch)
     MSByteDist = ch[0]
     LSByteDist = ch[1]
-    mmDist  = round((MSByteDist * 256 + LSByteDist)/10)
-    #print(round(mmDist/10))
-    return mmDist
+    cmDist  = round((MSByteDist * 256 + LSByteDist)/10)
+    return cmDist
 
 
 
-#while(True):
-for i in range(240):
+while(True):
+    while(recording == False):
+        pyb.LED(RED_LED_PIN).off()
+        #check for switch being pressed to start recording
+        if (pin0.value() == 1):
+            strrecordingsequence = str(recordingsequence)
+            m = mjpeg.Mjpeg("recording" + strrecordingsequence + ".mjpeg")
+            recordingsequence += 1
+            pyb.LED(RED_LED_PIN).on()
+            recording = True
+            print("Recording....")
+            pyb.delay(2000)
 
 
-    oor = False
-    distance = get_distance()
-    strdistance = str(distance)
-    clock.tick()
-
-    img = sensor.snapshot()
-
-    img.draw_rectangle(0, 0, 60, 40, color = (0, 0, 0), thickness = 0, fill = True)
 
 
-    if oor == False:
-        # Character and string rotation can be done at 0, 90, 180, 270, and etc. degrees.
-        img.draw_string(1, 1, strdistance, color = (255, 255, 255), scale = 2, mono_space = False,
-                        char_rotation = 0, char_hmirror = False, char_vflip = False,
-                        string_rotation = 0, string_hmirror = False, string_vflip = False)
+    while(recording == True):
+    #for i in range(240):
+
+
+        oor = False
+        distance = get_distance()
+
+        strdistance = str(distance)
+        #print(distancearray[1])
+        clock.tick()
+
+        img = sensor.snapshot()
+
+        if (distance>200):
+            oor = True
+
+
+
+        img.draw_rectangle(0, 0, 60, 40, color = (0, 0, 0), thickness = 0, fill = True)
+
+
+        if oor == False:
+            # Character and string rotation can be done at 0, 90, 180, 270, and etc. degrees.
+            img.draw_string(1, 1, strdistance, color = (255, 255, 255), scale = 2, mono_space = False,
+                            char_rotation = 0, char_hmirror = False, char_vflip = False,
+                            string_rotation = 0, string_hmirror = False, string_vflip = False)
+
+
+
+        if oor == True:
+            # Character and string rotation can be done at 0, 90, 180, 270, and etc. degrees.
+            img.draw_string(1, 1, "> 200", color = (255, 255, 255), scale = 2, mono_space = False,
+                            char_rotation = 0, char_hmirror = False, char_vflip = False,
+                            string_rotation = 0, string_hmirror = False, string_vflip = False)
+
+
 
         img.draw_string(37, 1, units, color = (255, 255, 255), scale = 2, mono_space = False,
                     char_rotation = 0, char_hmirror = False, char_vflip = False,
                     string_rotation = 0, string_hmirror = False, string_vflip = False)
 
 
-    m.add_frame(img)
-    #print(clock.fps())
+
+        m.add_frame(img)
+        #print(clock.fps())
 
 
-m.close(clock.fps())
-pyb.LED(BLUE_LED_PIN).off()
-print("Done! Reset the camera to see the saved recording.")
+        #check to see if switched pressed
+        if (pin0.value() == 0):
+            pyb.LED(RED_LED_PIN).off()
+            m.close(clock.fps())
+            print("Recording done - Reset the camera to see the saved recording.")
+            pyb.delay(2000)
+            recording = False
+
 
